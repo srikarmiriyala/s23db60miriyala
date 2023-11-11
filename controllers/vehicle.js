ce2d2aafe272f81e733e6dc4d844d8b6782bd249
srikarmiriyala <srikarmiriyala@gmail.com>
2023-11-10 18:59:07 -0600

    added vehicle changes

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -23,8 +23,24 @@ exports.vehicle_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: vehicle delete DELETE ' + req.params.id);
 };
 // Handle vehicle update form on PUT.
-exports.vehicle_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: vehicle update PUT' + req.params.id);
+exports.vehicle_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await vehicle.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.vehicle_type)
+    toUpdate.vehicle_type = req.body.vehicle_type;
+    if(req.body.vehicle_color) toUpdate.vehicle_color = req.body.vehicle_color;
+    if(req.body.vehicle_price) toUpdate.vehicle_price = req.body.vehicle_price;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
 };
 // List of all vehicles
 exports.vehicle_list = async function(req, res) {
@@ -50,14 +66,14 @@ res.status(500);
 res.send(`{"error": ${err}}`);
 }
 };
-// Handle Costume create on POST.
+// Handle vehicle create on POST.
 exports.vehicle_create_post = async function(req, res) {
     console.log(req.body)
     let document = new vehicle();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"vehicle_type":"goat", "cost":12, "size":"large"}
     document.vehicle_type = req.body.vehicle_type;
     document.vehicle_color = req.body.vehicle_color;
     document.vehicle_price = req.body.vehicle_price;
