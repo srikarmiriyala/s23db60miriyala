f4f3d732d043b72af5e482f4126d20e052719752
srikarmiriyala <srikarmiriyala@gmail.com>
2023-11-28 23:53:28 -0600

    added login

--- app.js
+++ app.js
@@ -4,16 +4,37 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
+  })
+)
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var vehicleRouter = require('./routes/vehicle');
 var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
-var vehicle = require('./models/vehicle');
+var vehicle = require("./models/vehicle");
+
 var resourceRouter = require('./routes/resource');
-
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -22,6 +43,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -31,35 +59,19 @@ app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 app.use('/resource', resourceRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 require('dotenv').config();
-const connectionString = process.env.MONGO_CON;
-const mongoose = require('mongoose');
+const connectionString =
+  process.env.MONGO_CON
+mongoose = require('mongoose');
 mongoose.connect(connectionString);
 
-//Get the default connection
 var db = mongoose.connection;
 //Bind connection to error event 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
-
-// We can seed the collection if needed on server start
+db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
 
 async function recreateDB(){
   // Delete everything
@@ -91,5 +103,26 @@ async function recreateDB(){
  }
  let reseed = true;
  if (reseed) { recreateDB();}
+
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
--- routes/vehicle.js
+++ routes/vehicle.js
@@ -7,6 +7,15 @@ router.get('/', function(req, res, next) {
 var express = require('express');
 const vehicle_controlers= require('../controllers/vehicle');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 /* GET vehicle */
 router.get('/', vehicle_controlers.vehicle_view_all_Page );
 /* GET detail vehicle page */
@@ -14,7 +23,7 @@ router.get('/detail', vehicle_controlers.vehicle_view_one_Page);
 /* GET create vehicle page */
 router.get('/create', vehicle_controlers.vehicle_create_Page);
 /* GET create update page */
-router.get('/update', vehicle_controlers.vehicle_update_Page);
+router.get('/update', secured, vehicle_controlers.vehicle_update_Page);
 /* GET delete vehicle page */
 router.get('/delete', vehicle_controlers.vehicle_delete_Page);
 
